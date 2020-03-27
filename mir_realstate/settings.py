318da03deb78035d72eaef56f405028e2b8ba7a0
Sohel Rana <sohelcuetcse11@gmail.com>
2020-03-27 22:29:54 +0600

    realtor and property added

--- mir_realstate/settings.py
+++ mir_realstate/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mir_realstate/static')
 ]
+
+#media folder settins
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
